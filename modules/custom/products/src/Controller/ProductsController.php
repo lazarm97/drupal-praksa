d380c6b4d89dbacdd47b309c807ab327cd1c487d
Lazar Marojkin <lmarojkin@gmail.com>
2021-03-25 22:46:41 +0100

    create filter by title

--- modules/custom/products/src/Controller/ProductsController.php
+++ modules/custom/products/src/Controller/ProductsController.php
@@ -5,23 +5,24 @@ use Drupal\file\Entity\File;
 
 class ProductsController{
   public function products(){
-    $image_local_path = "http://localhost/drupal-praksa/sites/default/files/";
-    $entity_type_manager = \Drupal::entityTypeManager();
-//    $nodes = Node::load();
-    $products_nodes = $entity_type_manager->getStorage('node')->loadByProperties(['type' => 'bike']);
-//    $image = $products_nodes[1]->getValue();
+    $entity_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $title_filter = $_GET['filter_title'];
+    $products_nodes_ids = $entity_storage->getQuery()
+      ->condition('type', 'bike')
+      ->condition('title', $title_filter, 'CONTAINS')
+      ->execute();
+    $products = $entity_storage->loadMultiple($products_nodes_ids);
     $products_info = array();
-    foreach ($products_nodes as $product){
+    foreach ($products as $product){
       $product = $product->toArray();
       $image_id = $product['field_image'][0]['target_id'];
       $image = File::load($image_id)->toArray();
       $image_uri = $image['uri'][0]['value'];
-      $image_uri = substr($image_uri,9);
-      $image_path = $image_local_path.$image_uri;
       $tmp_info = ['product' => [
         'title' => $product['field_title'][0]['value'],
         'description' => $product['body'][0]['value'],
-        'image' => $image_path
+        'image' => $image_uri,
+        'filter' => $title_filter
       ]];
       array_push($products_info,$tmp_info);
     }
